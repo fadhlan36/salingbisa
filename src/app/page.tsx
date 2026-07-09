90a7179f7bb3fc90e9e5ccc093ca74994fe9ca73
Alif Sufi Muhsinin <alifsufim@gmail.com>
2026-07-09 10:07:19 +0700

    feat(routing): redirect root path to dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,29 @@
 import { createClient } from "@/lib/supabase/server";
+import { redirect } from "next/navigation";
 
 export default async function Home() {
   const supabase = await createClient();
-  const { error } = await supabase.from("_test").select("*").limit(1);
+  // const { error } = await supabase.from("_test").select("*").limit(1);
 
-  return (
-    <section className="flex min-h-screen items-center justify-center">
-      <div className="text-center">
-        <h1 className="text-2xl font-bold">Kitabisa 🚀</h1>
-        <p className="text-sm text-muted-foreground mt-2">
-          Status koneksi Supabase:{" "}
-          {error ? `Ada pesan (${error.message})` : "Connected ✅"}
-        </p>
-      </div>
-    </section>
-  );
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
+  if (session) {
+    redirect("/dashboard");
+  }
+
+  redirect("/auth/login");
+
+  // return (
+  //   <section className="flex min-h-screen items-center justify-center">
+  //     <div className="text-center">
+  //       <h1 className="text-2xl font-bold">Kitabisa 🚀</h1>
+  //       <p className="text-sm text-muted-foreground mt-2">
+  //         Status koneksi Supabase:{" "}
+  //         {error ? `Ada pesan (${error.message})` : "Connected ✅"}
+  //       </p>
+  //     </div>
+  //   </section>
+  // );
 }
