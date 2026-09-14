6837c10050e63428533b38318e792b910c7f6125
Alif Sufi Muhsinin <alifsufim@gmail.com>
2026-09-14 13:59:22 +0700

    feat(profile): add data is_sender and match_status for identified user is match or no

--- src/app/api/user/profile/[username]/route.ts
+++ src/app/api/user/profile/[username]/route.ts
@@ -10,7 +10,7 @@ type Props = {
 };
 
 export async function GET(request: NextRequest, { params }: Props) {
-  const { error: authError } = authenticate(request);
+  const { user, error: authError } = authenticate(request);
 
   if (authError) {
     return authError;
@@ -38,6 +38,31 @@ export async function GET(request: NextRequest, { params }: Props) {
     return NextResponse.json({ message: error.message }, { status: 500 });
   }
 
+  let matchStatus: "none" | "pending" | "accepted" = "none";
+  let isSender = false;
+  let matchId: string | null = null;
+
+  // Cek relasi match antara user login dan target user jika user melihat dirinya sendiri skip ini
+  if (data!.id !== user!.userId) {
+    const { data: match, error: matchError } = await supabaseAdmin
+      .from("matches")
+      .select("id, status, sender_id")
+      .or(
+        `and(sender_id.eq.${user!.userId},receiver_id.eq.${data.id}),and(sender_id.eq.${data.id},receiver_id.eq.${user!.userId})`,
+      )
+      .maybeSingle();
+
+    if (matchError) {
+      return NextResponse.json({ error: matchError.message }, { status: 500 });
+    }
+
+    if (match) {
+      matchStatus = match.status === "accepted" ? "accepted" : "pending";
+      matchId = match.id;
+      isSender = match.sender_id === user!.userId;
+    }
+  }
+
   const teachSkill = data.user_skills
     .filter((item) => item.type === "teach")
     .map((item) => item.skills);
@@ -51,7 +76,14 @@ export async function GET(request: NextRequest, { params }: Props) {
   return NextResponse.json(
     {
       message: "Berhasil mengambil data profile",
-      data: { ...userData, skill_teach: teachSkill, skill_learn: learnSkill },
+      data: {
+        ...userData,
+        skill_teach: teachSkill,
+        skill_learn: learnSkill,
+        match_status: matchStatus,
+        is_sender: isSender,
+        match_id: matchId,
+      },
     },
     {
       status: 200,
